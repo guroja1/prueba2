78544c266017a6600e3c4fc73470e070e059c444
guroja1 <grcantill@gmail.com>
2016-02-11 20:40:58 -0600

    Merge origin/master
    Conflicts:
    	src/cr/ac/una/prograIII/appMVC/Vista/Server.java

--- SegundaPrueba/src/segundaprueba/SegundaPrueba.java
+++ SegundaPrueba/src/segundaprueba/SegundaPrueba.java
@@ -15,7 +15,7 @@ public class SegundaPrueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("segunda prueba");
     }
     
 }
